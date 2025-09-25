23ce8eddfe6d998411abc6a739bd0e835d8b9dbc
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-09-25 10:42:11 +0200

    Removed unused tanquery classes

--- apps/studio/data/vela/api-mappers.ts
+++ apps/studio/data/vela/api-mappers.ts
@@ -1,9 +1,14 @@
 import { components } from './vela-schema'
 import { Organization } from '../../types'
 import { Project } from '../projects/project-detail-query'
+import fs from 'node:fs'
+import CryptoJS from 'crypto-js'
 
 export type VelaOrganization = components['schemas']['Organization']
 export type VelaProject = components['schemas']['ProjectPublic']
+
+const isDocker = fs.existsSync('/.dockerenv')
+if (isDocker) console.log('Running in Docker, using fake encrypted connection string')
 
 export function mapOrganization(organization: VelaOrganization): Organization {
   return {
@@ -23,6 +28,13 @@ export function mapOrganization(organization: VelaOrganization): Organization {
 }
 
 export function mapProject(project: VelaProject): Project {
+  let encryptedConnectionString = project.encrypted_database_connection_string!
+  if (isDocker) {
+    encryptedConnectionString = CryptoJS.AES.encrypt(
+      'postgresql://supabase_admin:your-super-secret-and-long-postgres-password@db:5432/postgres', 'SAMPLE_KEY'
+    ).toString().trim() // FIXME: Encrypted connectionString needs to come from the outside
+  }
+
   return {
     id: project.id!,
     name: project.name!,
@@ -37,6 +49,6 @@ export function mapProject(project: VelaProject): Project {
     is_physical_backups_enabled: false,
     restUrl: '',
     subscription_id: '',
-    connectionString: project.encrypted_database_connection_string!,
+    connectionString: encryptedConnectionString,
   }
 }
