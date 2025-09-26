711291ca970c89dafc7c33980cf8aaa1b1d4bc27
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-09-26 15:58:04 +0200

    Further cleanup of shared-data

--- packages/shared-data/index.ts
+++ packages/shared-data/index.ts
@@ -1,19 +1,10 @@
-import config from './config'
 import extensions from './extensions.json'
 import logConstants from './logConstants'
-import { plans, PricingInformation } from './plans'
-import { pricing } from './pricing'
 import { products, PRODUCT_MODULES } from './products'
-import questions from './questions'
 
-export type { PricingInformation }
 export {
-  config,
   extensions,
   logConstants,
-  plans,
-  pricing,
   products,
   PRODUCT_MODULES,
-  questions,
 }
