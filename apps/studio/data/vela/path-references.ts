06cc7afbe9ced624eb73db188d5b9905e89accf4
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-10-01 08:30:50 +0200

    Fixed import mistake

--- apps/studio/data/vela/path-references.ts
+++ apps/studio/data/vela/path-references.ts
@@ -1,5 +1,6 @@
 import { getOrganizationSlug } from './organization-path-slug'
-import { getBranchRef, getProjectRef } from './project-path-ref'
+import { getProjectRef } from './project-path-ref'
+import { getBranchRef } from './branch-path-ref'
 
 export function getPathReferences(): { slug?: string, ref?: string, branch?: string} {
   return {
