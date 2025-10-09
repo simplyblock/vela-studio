920b264bb65551f469deb2b132d86b00b1059d00
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-10-09 14:54:19 +0200

    Reworked auth providers to use keycloak proxy

--- apps/studio/pages/api/platform/organizations/[slug]/projects/[ref]/branches/[branch]/auth/providers/[name]/index.ts
+++ apps/studio/pages/api/platform/organizations/[slug]/projects/[ref]/branches/[branch]/auth/providers/[name]/index.ts
@@ -6,13 +6,13 @@ import { getPlatformQueryParams } from 'lib/api/platformQueryParams'
 const handleDelete = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, ref, branch, name } = getPlatformQueryParams(req, "slug", "ref", "branch", "name")
   const client = getVelaClient(req)
-  return client.proxyDelete(res, '/organizations/{organization_id}/projects/{project_id}/branches/{branch_id}/auth/oauth/{oauth_name}/', {
+  return client.proxyDelete(res, '/organizations/{organization_id}/projects/{project_id}/branches/{branch_id}/auth/identity-provider/instances/{alias}', {
     params: {
       path: {
         organization_id: slug,
         project_id: ref,
         branch_id: branch,
-        oauth_name: name,
+        alias: name,
       }
     }
   })
--- apps/studio/pages/api/platform/organizations/[slug]/projects/[ref]/branches/[branch]/auth/providers/index.ts
+++ apps/studio/pages/api/platform/organizations/[slug]/projects/[ref]/branches/[branch]/auth/providers/index.ts
@@ -1,22 +1,46 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { apiBuilder } from 'lib/api/apiBuilder'
-import { getVelaClient } from 'data/vela/vela'
+import { proxyWithMapping } from 'data/vela/vela'
 import { getPlatformQueryParams } from 'lib/api/platformQueryParams'
+import { deepObjectFilter } from 'lib/api/apiFilters'
 
 const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { slug, ref, branch } = getPlatformQueryParams(req, "slug", "ref", "branch")
-  const client = getVelaClient(req)
-  return client.proxyGet(res, '/organizations/{organization_id}/projects/{project_id}/branches/{branch_id}/auth/oauth/', {
-    params: {
-      path: {
-        organization_id: slug,
-        project_id: ref,
-        branch_id: branch
-      }
+  const { slug, ref, branch } = getPlatformQueryParams(req, 'slug', 'ref', 'branch')
+
+  return await proxyWithMapping(
+    req,
+    res,
+    'get',
+    '/organizations/{organization_id}/projects/{project_id}/branches/{branch_id}/auth/identity-provider/instances',
+    (value) => {
+      if (value === undefined) return undefined
+      return value.map((item) => {
+        return deepObjectFilter(item, [
+          'internalId',
+          'updateProfileFirstLoginMode',
+          'storeToken',
+          'addReadTokenRoleOnCreate',
+          'authenticateByDefault',
+          'hideOnLogin',
+          'firstBrokerLoginFlowAlias',
+          'postBrokerLoginFlowAlias',
+          'organizationId',
+          'updateProfileFirstLogin',
+        ])
+      })
+    },
+    {
+      params: {
+        path: {
+          organization_id: slug,
+          project_id: ref,
+          branch_id: branch,
+        },
+      },
     }
-  })
+  )
 }
 
-const apiHandler = apiBuilder(builder => builder.useAuth().get(handleGet))
+const apiHandler = apiBuilder((builder) => builder.useAuth().get(handleGet))
 
 export default apiHandler
