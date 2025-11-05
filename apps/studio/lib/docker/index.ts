4c38d36dec36ba38fde9c04d4bc7aa1fba69663f
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-11-05 10:37:52 +0100

    Updated docker detection

--- apps/studio/lib/docker/index.ts
+++ apps/studio/lib/docker/index.ts
@@ -1,10 +1,17 @@
 import fs from 'node:fs'
 
-const DEV_MODE = typeof process.env.DEV_MODE !== undefined;
+const DEV_MODE = typeof process.env.DEV_MODE !== 'undefined';
 
 export function isDocker() {
-  if (!DEV_MODE) return false;
+  if (!DEV_MODE) {
+    console.log('Dev mode not enabled, skipping docker check')
+    return false;
+  }
   const isDocker = fs.existsSync('/.dockerenv')
-  if (isDocker) console.log('Running in Docker, using fake encrypted connection string')
+  if (isDocker) {
+    console.log('Running in Docker, using fake encrypted connection string')
+  } else {
+    console.log('Checked for Docker, but not found, using real encrypted connection string')
+  }
   return isDocker;
 }
