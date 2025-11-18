6aec6e7130127a48892906b627ad611fc68fb977
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-11-18 12:21:41 +0100

    Added missing orgs member delete

--- apps/studio/pages/api/platform/organizations/[slug]/members/invitations/[id]/index.ts
+++ apps/studio/pages/api/platform/organizations/[slug]/members/invitations/[id]/index.ts
@@ -1,9 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { apiBuilder } from 'lib/api/apiBuilder'
+import { getPlatformQueryParams } from 'lib/api/platformQueryParams'
+import { getVelaClient } from 'data/vela/vela'
 
-// FIXME: Missing implementation
 const handleDelete = (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json({})
+  const { slug, id } = getPlatformQueryParams(req, 'slug', 'id')
+  const client = getVelaClient(req)
+  return client.proxyDelete(res, '/organizations/{organization_id}/members/{user_id}', {
+    params: {
+      path: {
+        organization_id: slug,
+        user_id: id,
+      },
+    },
+  })
 }
 
 const apiHandler = apiBuilder((builder) => builder.useAuth().delete(handleDelete))
