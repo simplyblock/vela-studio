ecbf4d273db0a147d43d32cefdd48c88d5e7b879
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-12-22 09:17:35 +0100

    Added metering proxy and query class

---
+++ apps/studio/data/resources/organization-metering-query.ts
@@ -0,0 +1,61 @@
+import { get, handleError } from '../fetchers'
+import {
+  type OmitKeyof,
+  QueryFunctionContext,
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query'
+import { ResponseError } from '../../types'
+import { resourcesKeys } from './keys'
+
+interface OrganizationUsageVariables {
+  orgRef?: string
+  start?: string
+  end?: string
+}
+
+async function getOrganizationUsage(
+  { orgRef, start, end }: OrganizationUsageVariables,
+  signal?: AbortSignal
+) {
+  if (!orgRef) throw new Error('Organization slug is required')
+
+  const { data, error } = await get('/platform/organizations/{slug}/metering', {
+    params: {
+      path: {
+        slug: orgRef,
+      },
+      query: {
+        start,
+        end,
+      },
+    },
+    signal,
+  })
+
+  if (error) handleError(error)
+  return data
+}
+
+export type OrganizationUsageData = Awaited<ReturnType<typeof getOrganizationUsage>>
+export type OrganizationUsageError = ResponseError
+
+export const useOrganizationUsageQuery = <TData = OrganizationUsageData>(
+  { orgRef, start, end }: OrganizationUsageVariables,
+  {
+    enabled = true,
+    ...options
+  }: OmitKeyof<
+    UseQueryOptions<OrganizationUsageData, OrganizationUsageError, TData>,
+    'initialData'
+  > = {}
+) => {
+  return useQuery<OrganizationUsageData, OrganizationUsageError, TData>({
+    ...options,
+    queryKey: resourcesKeys.organizationUsage(orgRef), // FIXME: @Chris do we want to cache this?
+    staleTime: 60000,
+    queryFn: async (context: QueryFunctionContext) =>
+      getOrganizationUsage({ orgRef, start, end }, context.signal),
+    enabled: enabled && typeof orgRef !== 'undefined',
+  })
+}
