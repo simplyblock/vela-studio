ec1ebc80228556f26ede2a75110f318a83221fdc
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-23 08:18:07 +0100

    Removed unused code in apps/studio

--- apps/studio/components/interfaces/ProjectCreation/ProjectCreation.utils.ts
+++
@@ -1,9 +0,0 @@
-const smartRegionToExactRegionMap = new Map([
-  ['Americas', 'East US (North Virginia)'],
-  ['Europe', 'Central EU (Frankfurt)'],
-  ['APAC', 'Southeast Asia (Singapore)'],
-])
-
-export function smartRegionToExactRegion(smartOrExactRegion: string) {
-  return smartRegionToExactRegionMap.get(smartOrExactRegion) ?? smartOrExactRegion
-}
