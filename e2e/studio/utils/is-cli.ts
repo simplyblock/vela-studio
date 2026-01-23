7742db99dc1cdb0bab31c4f5d4959414ed70c102
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-23 08:38:39 +0100

    Removed e2e files

--- e2e/studio/utils/is-cli.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '../env.config'
-
-/**
- * Returns true if running in CLI/self-hosted mode (locally),
- * false if running in hosted mode.
- */
-export function isCLI(): boolean {
-  return false
-}
